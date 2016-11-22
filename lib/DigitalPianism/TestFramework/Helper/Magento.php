0b393e75739aa97d69f0974bd337dec3176bac9c
Simon Frost <s.frost@frostnet.co.uk>
2016-11-22 12:35:14 +0000

    Updated include paths

--- lib/DigitalPianism/TestFramework/Helper/Magento.php
+++ lib/DigitalPianism/TestFramework/Helper/Magento.php
@@ -1,40 +1,49 @@
 <?php
 
+/**
+ * Class DigitalPianism_TestFramework_Helper_Magento
+ * @link http://magento.stackexchange.com/questions/143976/best-practice-for-unit-tests-in-magento-1-9
+ */
 class DigitalPianism_TestFramework_Helper_Magento
 {
-    private function patchMagentoAutoloader()
-    {
-        $mageErrorHandler = set_error_handler(function () {
-            return false;
-        });
-        set_error_handler(function ($errno, $errstr, $errfile) use ($mageErrorHandler) {
-            if (substr($errfile, -19) === 'Varien/Autoload.php') {
-                return null;
-            }
-            return is_callable($mageErrorHandler) ?
-                call_user_func_array($mageErrorHandler, func_get_args()) :
-                false;
-        });
-    }
+	private static function patchMagentoAutoloader()
+	{
+		$mageErrorHandler = set_error_handler(function () {
+			return false;
+		});
+		set_error_handler(function ($errno, $errstr, $errfile) use ($mageErrorHandler) {
+			if (substr($errfile, -19) === 'Varien/Autoload.php') {
+				return null;
+			}
+			return is_callable($mageErrorHandler) ?
+				call_user_func_array($mageErrorHandler, func_get_args()) :
+				false;
+		});
+	}
 
-    public static function bootstrap()
-    {
-        require __DIR__ . '/../../../../app/Mage.php';
-        self::patchMagentoAutoloader();
-        self::init();
-    }
+	/**
+	 * Modified path to Mage.php here because PHP automatically follows symlinks in require(),
+	 * which means this path is actually relative to the vendor folder, not the www folder.
+	 * This path assumes your extension source code is in <PROJECT_ROOT>/src and Magento code is in <PROJECT_ROOT>/www
+	 */
+	public static function bootstrap()
+	{
+		require __DIR__ . '/../../../../../../../www/app/Mage.php';
+		self::patchMagentoAutoloader();
+		self::init();
+	}
 
-    public function reset()
-    {
-        Mage::reset();
-        self::init();
-    }
-    
-    public static function init()
-    {
-        Mage::app('', 'store', ['config_model'  =>  DigitalPianism_TestFramework_Model_Config::class]);
-        Mage::setIsDeveloperMode(true);
-        self::patchMagentoAutoloader();
-        $_SESSION = [];
-    }
+	public function reset()
+	{
+		Mage::reset();
+		self::init();
+	}
+
+	public static function init()
+	{
+		Mage::app('', 'store', ['config_model'  =>  DigitalPianism_TestFramework_Model_Config::class]);
+		Mage::setIsDeveloperMode(true);
+		self::patchMagentoAutoloader();
+		$_SESSION = [];
+	}
 }
--- lib/DigitalPianism/TestFramework/bootstrap.php
+++ lib/DigitalPianism/TestFramework/bootstrap.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @link http://magento.stackexchange.com/questions/143976/best-practice-for-unit-tests-in-magento-1-9
+ */
 
-require __DIR__ . '/Helper/Magento.php';
+// This path assumes this file is installed inside the webroot
+//require __DIR__ . '/../../../../../../lib/DigitalPianism/TestFramework/Helper/Magento.php';
+
+// This path assumes this file is stored outside the webroot and symlinked in
+require __DIR__ . '/../../../../../../../www/lib/DigitalPianism/TestFramework/Helper/Magento.php';
 DigitalPianism_TestFramework_Helper_Magento::bootstrap();
